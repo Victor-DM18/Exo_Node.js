6c8259d99d1907403df9c4a78d049bab1f16df9d
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-04-15 17:55:43 +0200

    feat : connection MongoDB & .env

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const connectDb = require("./config /db");
+const dotenv = require("dotenv").config();
 const port = 5008;
+
+connectDb();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/post", require("./routes/postRoute"));
 
